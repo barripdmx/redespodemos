8f34865636a7b713ac0545e0704a7e7e4d168a3d
Barri <barripdmx@gmail.com>
2017-08-31 13:33:28 +0200

    Add files via upload

--- gexf/config.js
+++ gexf/config.js
@@ -1,18 +1,20 @@
 /*** USE THIS FILE TO SET OPTIONS ***/
 
-setParams({
-    graphFile : "echararajoy.gexf",
+GexfJS.setParams({
+    graphFile : "miserables.gexf",
         /*
             The GEXF file to show ! -- can be overriden by adding
             a hash to the document location, e.g. index.html#celegans.gexf
+            GEXF files can now be replaced by pre-processed JSON files (use gexf2json.py)
+            for faster load time
         */
 	showEdges : true,
         /*
-            Default state of the "show edges" button
+            Default state of the "show edges" button. Set to null to disable button.
         */
     useLens : false,
         /*
-            Default state of the "use lens" button
+            Default state of the "use lens" button. Set to null to disable button.
         */
     zoomLevel : 0,
         /*
@@ -30,6 +32,7 @@ setParams({
         */
     minEdgeWidth : 1,
     maxEdgeWidth : 50,
+    textDisplayThreshold: 9,
     nodeSizeFactor : 1,
         /*
             Change this parameter for smaller or larger nodes
@@ -45,11 +48,34 @@ setParams({
             Show the weight of edges in the list
             this setting can't be changed from the User Interface
         */
-    language: es
+    showEdgeLabel : true,
+    sortNodeAttributes: true,
+        /*
+            Alphabetically sort node attributes
+         */
+    showId : true,
+        /*
+            Show the id of the node in the list
+            this setting can't be changed from the User Interface
+        */
+    showEdgeArrow : true,
+        /*
+            Show the edge arrows when the edge is directed
+            this setting can't be changed from the User Interface
+        */
+    language: false,
         /*
             Set to an ISO language code to switch the interface to that language.
-            Available languages are English [en], French [fr], Spanish [es],
-            Italian [it], Finnish [fi], Turkish [tr] and Greek [el].
+            Available languages are:
+            - German [de]
+            - English [en]
+            - French [fr]
+            - Spanish [es]
+            - Italian [it]
+            - Finnish [fi]
+            - Turkish [tr]
+            - Greek [el]
+            - Dutch [nl]
             If set to false, the language will be that of the user's browser.
         */
 });
